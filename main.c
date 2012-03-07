86bfc1bb85caabab4a97f84dbce4bbb700700fad
Alex Palex <palexster@gmail.com>
2012-03-07 16:37:06 +0100

    Prova!

---
+++ main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: alex
+ *
+ * Created on 7 marzo 2012, 16.28
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
