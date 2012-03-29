58a4c8245ffcbe24990d36a8cfde6219a93cdfea
Alex Palex <palexster@gmail.com>
2012-03-29 16:59:54 +0200

    Pull for speed!!!

--- nbproject/Makefile-impl.mk
+++ nbproject/Makefile-impl.mk
@@ -24,7 +24,7 @@ CLEAN_SUBPROJECTS=${CLEAN_SUBPROJECTS_${SUBPROJECTS}}
 
 
 # Project Name
-PROJECTNAME=genetic
+PROJECTNAME=Genetic
 
 # Active Configuration
 DEFAULTCONF=Debug
--- popolation.c
+++ popolation.c
@@ -2,10 +2,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <math.h>
 #include "pieces.h"
 #include "popolation.h"
-//boolean val
-enum{FALSE,TRUE};
 
 /*Funzione che crea la popolazione*/
 population_t *build_population(int **pieces,int npieces,int row,int col){
@@ -24,7 +23,15 @@ population_t *build_population(int **pieces,int npieces,int row,int col){
     return popolazione_start;
 }
 
+/*
+ * Funzione diagnostica che stampa i valori di fitness di tutte le soluzioni di 
+ * una popolazione e ne calcola la migliore con rispettiva fitness.
+ * Ne calcola inoltre la varianza e la media.
+ */
+
 void test_fitness(population_t *pop){
+    int totale=0;
+    float media,varianza;
     int i,max=0,idmax=0;
     //printf("Qual è la miglior soluzione?\n");
     for(i=0;i<POP_DIM;i++){
@@ -32,8 +39,15 @@ void test_fitness(population_t *pop){
             max=pop->soluzioni[i].fitness;
             idmax=i;
         }
+        totale+=pop->soluzioni[i].fitness;
         //printf("Soluzione %d:\t %d \n",i,pop->soluzioni[i].fitness);
     }
+    media =(float) totale / POP_DIM;
+    for(i=0;i<POP_DIM;i++){
+    varianza += pow((pop->soluzioni[i].fitness - media),2); 
+    }
+    varianza = varianza / POP_DIM;
+    printf("Media Popolazione: %f \t Varianza Popolazione: %f\n",media,varianza);
     printf("Miglior Soluzione n° %d , punti: %d\n",idmax,pop->soluzioni[idmax].fitness);
 }
 
@@ -81,7 +95,7 @@ void dealloc_population(population_t *pop,int row){
     return;
 }
 
-void pop_evolution(int *pieces,int npieces,population_t *pop,int row, int col){
+void pop_evolution(int **pieces,int npieces,population_t *pop,int row, int col){
     char chosen[POP_DIM];//vettore di flag per tenere conto che la sol i è già 
                          //estratta (o scelta come genitore o scelta per essere
                          //come genitore)
@@ -122,6 +136,91 @@ void pop_evolution(int *pieces,int npieces,population_t *pop,int row, int col){
                         tmp++;
                 gen[cntgen]=tmp;
         }
-        crossover(pop[gen[1]],pop[gen[2]]],&offspring[i++],&offspring[i++]);
+        crossover(pieces,&pop->soluzioni[gen[1]],&pop->soluzioni[gen[2]],&offspring[i++],&offspring[i++],npieces,row,col);
+    }
+}
+
+void crossover(int **pieces,solution_t *sol1, solution_t *sol2, solution_t *fig1,solution_t *fig2, int npieces, int row, int col){
+    // generazione tagli, contatori e indice righe/colonne
+    int taglio1,taglio2,i,r,c,c1,r1,j,flag;
+    // confronto pezzi dentro il kernel
+    char pezzoDaControllare,**kernelPieces;
+    //allocazione vettori per il confronto ottimizzato del kernel
+    kernelPieces=(char **)malloc(sizeof(char*)*npieces);
+    for(i=0;i<npieces;i++){
+        kernelPieces[i]=(char *)malloc(sizeof(char)*2);
+        kernelPieces[i][0]=-1;
+        kernelPieces[i][1]=-1;
+    }
+    taglio1=rand() % npieces/2 +1 ;
+    taglio2=rand() % npieces/2 + npieces/2;
+    if (taglio2=taglio1)
+        taglio2=taglio2 + npieces/10;
+    *fig1=build_solution(pieces,row,col);
+    *fig2=build_solution(pieces,row,col);
+    // Generazione kernel della prole
+    for(i=taglio1;i<taglio2;i++){
+        r = i/col;
+        c = i % col;
+        // figlio 1
+        fig1->matrice_pezzi[r][c][0]=sol1->matrice_pezzi[r][c][0];
+        fig1->matrice_pezzi[r][c][1]=sol1->matrice_pezzi[r][c][1];
+        kernelPieces[sol1->matrice_pezzi[r][c][0]][0]=i;
+        // figlio 2
+        fig2->matrice_pezzi[r][c][0]=sol2->matrice_pezzi[r][c][0];
+        fig2->matrice_pezzi[r][c][1]=sol2->matrice_pezzi[r][c][1];
+        kernelPieces[sol2->matrice_pezzi[r][c][0]][1]=i;
+    }
+    // Generazione lato sinistro della prole
+    for(i=0;i<taglio1;i++){
+        r = i/col;
+        c = i % col;
+        flag=0;
+        j=taglio1;
+        // se il pezzo non è già presente nel kernel
+        if (kernelPieces[sol1->matrice_pezzi[r][c][0]][0]<0)
+            fig1->matrice_pezzi[r][c][0]=sol2->matrice_pezzi[r][c][0];
+        // se il pezzo è già presente nel kernel
+        else {
+            j=kernelPieces[sol1->matrice_pezzi[r][c][0]][0];
+            r1 = j/col;
+            c1 = j % col;
+            fig1->matrice_pezzi[r][c][0]=sol2->matrice_pezzi[r1][c1][0];
+        if (kernelPieces[sol2->matrice_pezzi[r][c][0]][1]<0)
+            fig2->matrice_pezzi[r][c][0]=sol1->matrice_pezzi[r][c][0];
+        // se il pezzo è già presente nel kernel
+        else {
+            j=kernelPieces[sol2->matrice_pezzi[r][c][0]][1];
+            r1 = j/col;
+            c1 = j % col;
+            fig2->matrice_pezzi[r][c][0]=sol1->matrice_pezzi[r1][c1][0];
+        }
+        }
+    }
+    //Generazione lato destro della prole
+    for(i=taglio2;i<npieces;i++){
+        r = i/col;
+        c = i % col;
+        flag=0;
+        j=taglio1;
+        // se il pezzo non è già presente nel kernel
+        if (kernelPieces[sol1->matrice_pezzi[r][c][0]][0]<0)
+            fig1->matrice_pezzi[r][c][0]=sol2->matrice_pezzi[r][c][0];
+        // se il pezzo è già presente nel kernel
+        else {
+            j=kernelPieces[sol1->matrice_pezzi[r][c][0]][0];
+            r1 = j/col;
+            c1 = j % col;
+            fig1->matrice_pezzi[r][c][0]=sol2->matrice_pezzi[r1][c1][0];
+        if (kernelPieces[sol2->matrice_pezzi[r][c][0]][1]<0)
+            fig2->matrice_pezzi[r][c][0]=sol1->matrice_pezzi[r][c][0];
+        // se il pezzo è già presente nel kernel
+        else {
+            j=kernelPieces[sol2->matrice_pezzi[r][c][0]][1];
+            r1 = j/col;
+            c1 = j % col;
+            fig2->matrice_pezzi[r][c][0]=sol1->matrice_pezzi[r1][c1][0];
+        }
+        }
     }
 }
