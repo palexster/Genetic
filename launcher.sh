5afc7bc3bab909ee71b9f6e92c7acd7ffb66a2d3
palexster <palexster@gmail.com>
2012-05-10 16:12:28 +0200

    rinascita #2

---
+++ launcher.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+date>a1
+./dist/Debug/GNU-Linux-x86/genetic_gold pieces_05x05.txt Output05x05.txt Statistiche05x05.txt
+date>a3
+sleep 360
+date>a4
+./dist/Debug/GNU-Linux-x86/genetic_gold pieces_10x10.txt Output10x10.txt Statistiche10x10.txt
+date>a5
+sleep 360
+date>a4
+./dist/Debug/GNU-Linux-x86/genetic_gold pieces_05x05.txt Output05x05_2.txt Statistiche05x05_2.txt
+date
+sleep 360
+date>a5
+./dist/Debug/GNU-Linux-x86/genetic_gold pieces_10x10.txt Output10x10.txt Statistiche10x10_2.txt
+date>a6
+sleep 360
+date>a7
+./dist/Debug/GNU-Linux-x86/genetic_gold pieces_07x07.txt Output07x07.txt Statistiche07x07.txt
+date>a8
+sleep 360
+date>a9
+./dist/Debug/GNU-Linux-x86/genetic_gold pieces_12x12.txt Output_12x12 Statistiche12x12.txt
+date>a10
