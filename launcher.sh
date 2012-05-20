f6d7654c1732dabd8d7ec6e8703564280ff02670
Iacopo <iacopo.va@gmail.com>
2012-05-20 13:08:05 +0200

    stiamo attenti con git!nelle varie operazioni era andata persa la modifica della test evolution che avevo fatto (perchè mancavano le iniziallizzazioni di totale e varianza), e ho dovuto di nuovo rifarle.

--- launcher.sh
+++ launcher.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 date>tst1
-./dist/Release/GNU-Linux-x86/genetic pieces_08x08.txt out10x10_1.txt 100 100000 --debug
+./dist/Release/GNU-Linux-x86/genetic pieces_08x08.txt out10x10_1.txt 100 20000 --debug
 sleep 360
 date>tst2
-./dist/Release/GNU-Linux-x86/genetic pieces_08x08.txt out08x08_2.txt 100 100000 --debug
+./dist/Release/GNU-Linux-x86/genetic pieces_08x08.txt out08x08_2.txt 100 100000 
 sleep 360
 date>tst3
-./dist/Release/GNU-Linux-x86/genetic pieces_08x08.txt out08x08_3.txt 250 200000 --debug
+./dist/Release/GNU-Linux-x86/genetic pieces_08x08.txt out08x08_3.txt 250 200000 
 
 
